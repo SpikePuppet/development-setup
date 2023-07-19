3c51b81d0222a6563fa87f4ff00afc44092d6e1d
Rhys Johns <rhysjohnsdev@gmail.com>
2023-07-19 23:09:34 +1000

    Added harpoon

--- .config/nvim/lua/spikepuppet/packer.lua
+++ .config/nvim/lua/spikepuppet/packer.lua
@@ -133,6 +133,8 @@ return require('packer').startup(function(use)
     }
     use {'rrethy/vim-illuminate'}
     use {'folke/todo-comments.nvim'}
+    use {'theprimeagen/harpoon'}
+
     if packer_bootstrap then
         require('packer').sync()
     end
